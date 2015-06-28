d2bf6e9f331e58ccf7606181219a6e460c712e92
towry <tovvry@gmail.com>
2015-06-28 14:38:48 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
+    './scripts/index'
+  ],
+  devServer: {
+    contentBase: './build',
+    hot: true,
+    inline: true
+  },
+  devtool: 'eval-source-map',
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'scripts')
+    }]
+  }
+}
